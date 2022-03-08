59c90b20833124367aeed6591b7dc56dbcc11f6f
Tomer Burman <tomerbu1@ac.sce.ac.il>
2022-03-08 19:23:25 +0200

    Initial commit

---
+++ machine_precision.py
@@ -0,0 +1,11 @@
+def machine_precision():
+    '''
+    finds the machine precision in current environment (32bit/64bit)
+    :return: none
+    '''
+    x, temp, eps = 1, 0, 0.0
+    while (x + 1 != 1):
+        eps = x
+        x /= 2
+        temp += 1
+    print(f"Smallest number: {eps}, iterations: {temp}")
